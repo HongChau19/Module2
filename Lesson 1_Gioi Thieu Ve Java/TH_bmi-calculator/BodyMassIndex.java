89221bc8ccbd8b618044d407a312e32fbbb89491
HongChau19 <vohongchau20@gmail.com>
2025-05-09 20:38:08 +0700

    update

--- Lesson 1_Gioi Thieu Ve Java/TH_bmi-calculator/BodyMassIndex.java
+++ Lesson 1_Gioi Thieu Ve Java/TH_bmi-calculator/BodyMassIndex.java
@@ -1,21 +1,28 @@
 import java.util.Scanner;
+
 public class BodyMassIndex {
     public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     double weight, height, bmi;
-        System.out.print("Your weight (in kilogram):");
+    System.out.print("Cân nặng của bạn (tính bằng kilogram):");
+
     weight = scanner.nextDouble();
-        System.out.print("Your height (in meter):");
+
+    System.out.print("Chiều cao của bạn (tính bằng mét):");
+
     height = scanner.nextDouble();
+
     bmi = weight / Math.pow(height, 2);
-        System.out.printf("%-20s%s", "bmi", "Interpretation\n");
+
+    System.out.printf("%-20s%s", "bmi", "Diễn giải\n");
 
         if (bmi < 18)
-            System.out.printf("%-20.2f%s", bmi, "Underweight");
+            System.out.printf("%-20.2f%s", bmi, "Thiếu cân");
         else if (bmi < 25.0)
-            System.out.printf("%-20.2f%s", bmi, "Normal");
+            System.out.printf("%-20.2f%s", bmi, "Bình thường");
         else if (bmi < 30.0)
-            System.out.printf("%-20.2f%s", bmi, "Overweight");
+            System.out.printf("%-20.2f%s", bmi, "Thừa cân");
         else
                 System.out.printf("%-20.2f%s", bmi, "Obese");
-}
+    }
+    }
