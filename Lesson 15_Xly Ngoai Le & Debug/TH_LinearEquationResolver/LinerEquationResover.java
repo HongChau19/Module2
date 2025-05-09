613f505b71d98c90cae3aa2484b96c2fa3c84d6b
HongChau19 <vohongchau20@gmail.com>
2025-05-09 20:40:58 +0700

    cap nhat

--- Lesson 15_Xly Ngoai Le & Debug/TH_LinearEquationResolver/LinerEquationResover.java
+++ Lesson 15_Xly Ngoai Le & Debug/TH_LinearEquationResolver/LinerEquationResover.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class LinerEquationResover {
+    public static void main(String[] args) {
+        System.out.println("Liner Equation Resolver:");
+        System.out.println("Give a equation as 'a * x + b = 0', please: ");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("a: ");
+        double a = scanner.nextDouble();
+
+        System.out.print("b: ");
+        double b = scanner.nextDouble();
+
+        if (a != 0) {
+
+        }
+
+
+    }
 }
